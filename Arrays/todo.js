354993417c9358dc69a44dd7e079a1c9712eb4cf
Jake-woods <jake.13.5.2016@gmail.com>
2019-01-16 13:36:35 +0000

    Todo commit

--- Arrays/todo.js
+++ Arrays/todo.js
@@ -1,10 +1,38 @@
 // Challenge file
-const todos = ['Clean', 'Sleep', 'Eat'];
+const todos = [{
+      text: 'Todo 1',
+      completed: false
+   },
+   {
+      text: 'Todo 2',
+      completed: false
+   },
+   {
+      text: 'Todo 3',
+      completed: true
+   },
+   {
+      text: 'Todo 4',
+      completed: false
+   },
+   {
+      text: 'Todo 5',
+      completed: true
+   },
+];
+// Function to
+// Find index using findIndex (returns the index of item if found)
+// then using that value to remove the item
+const removeTodo = (todos, todoText) => {
+   const index = todos.findIndex((todo) => {
+      return todo.text.toLowerCase() === todoText.toLowerCase();
+   })
 
-// For loop
-todos.forEach((todo, index) => console.log(`${index + 1}. ${todo}`));
-
-// For
-for (let i = 0; i < todos.length; i++) {
-   console.log(`${i + 1}. ${todos[i]}`);
+   // If found
+   if (index > -1) {
+      todos.splice(index, 1);
+   }
 }
+
+removeTodo(todos, 'Todo 3');
+console.log(todos);
