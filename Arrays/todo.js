2eff47ed93db3d32295fbd6d1e0f3cca7f5f64ad
Jake-woods <jake.13.5.2016@gmail.com>
2019-01-16 12:10:26 +0000

    Arrays commit

---
+++ Arrays/todo.js
@@ -0,0 +1,6 @@
+// Challenge file
+const todos = ['Clean', 'Sleep', 'Eat'];
+
+console.log(`You have ${todos.length} todos`);
+console.log(`First todo: ${todos[0]}`);
+console.log(`Last todo: ${todos[todos.length - 1]}`);
