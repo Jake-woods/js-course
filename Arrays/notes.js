35fc2c91038037e696b4b6b1054b97df504a0c53
Jake-woods <jake.13.5.2016@gmail.com>
2019-01-16 12:30:35 +0000

    Notes

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -1,16 +1,3 @@
 const notes = ['Note 1', 'Note 2', 'Note 3'];
 
-// Remove last
-notes.pop();
-// Add to end
-notes.push('Note 4');
-// Removes first item
-notes.shift();
-// Adds to start
-notes.unshift('New first note');
-
-// where, number of things removing, add item in that localation
-notes.splice(1, 0, 'New item');
-
-
-console.log(notes);
+notes.forEach((note, index) => console.log(note));
