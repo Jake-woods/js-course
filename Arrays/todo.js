6504eaaa753ece663cc5c8cda2e64abdafdadafa
Jake-woods <jake.13.5.2016@gmail.com>
2019-01-16 12:33:42 +0000

    Todo commit

--- Arrays/todo.js
+++ Arrays/todo.js
@@ -1,8 +1,4 @@
 // Challenge file
 const todos = ['Clean', 'Sleep', 'Eat'];
 
-todos.splice(2, 1);
-todos.push('New item');
-todos.shift();
-
-console.log(todos);
+todos.forEach((todo, index) => console.log(`${index + 1}. ${todo}`));
